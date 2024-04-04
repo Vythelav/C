fd2425e0f230bd3b10d384e2e70d35beb6495d96
Vythelav <slava.gusaov@gmail.com>
2024-04-04 14:12:43 +0300

    creeate

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+
+        //int a = 2;
+        //int b = 2;
+        //int c = a + b;
+        //int d = a - b;
+        //int g = a * b;
+        //int f = a / b;
+        //string One = c.ToString();
+        //string Two = d.ToString();
+        //string Th = g.ToString();
+        //string fore = f.ToString();
+        //Console.WriteLine(One + " " + Two + " " + Th + " " + fore);
+
+
+        double l;
+        l = Convert.ToDouble(Console.ReadLine());
+        double s, r;
+        r = l / (2 * 3.14);
+        s = 3.14 * r * r;
+        Console.WriteLine("S={0}  ", s);
+    }
+}
